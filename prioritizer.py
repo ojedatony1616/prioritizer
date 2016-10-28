7017e526db85c5c3c8eac2e03d77462a7d4a8dde
Tony Ojeda <tojeda@districtdatalabs.com>
2016-10-28 12:45:24 -0400

    Uploading initial code

---
+++ prioritizer.py
@@ -0,0 +1,39 @@
+#usr/bin/env/python
+
+import random
+from random import shuffle
+import itertools
+from operator import itemgetter
+
+#Initialize the list
+items = []
+
+#Take input from user for items to add to priority list
+item = 0
+
+while item != "":
+	item = input("Enter item to prioritize (leave blank when done): ")
+	if item != "":
+		items.append(item)
+
+#Initialize a dict to keep score
+scores = {x : 0 for x in items}
+
+#Get all the combinations and save them to a list
+combos = list(itertools.combinations(items, r=2))
+shuffle(combos)
+
+for i in range(len(combos)):
+	current = combos[i]
+	option_1 = current[0]
+	option_2 = current[1]
+	answer = input(str(option_1) + " or " + str(option_2) + "? ")
+	while answer not in items:
+		answer = input(str(option_1) + " or " + str(option_2) + "? ")
+	scores[answer] = scores.get(answer, 0) + 1
+
+scores = sorted(scores.items(), key=itemgetter(1), reverse=True)
+
+for x in scores:
+	print(str(x[0]) + ": " + str(x[1]))
+
